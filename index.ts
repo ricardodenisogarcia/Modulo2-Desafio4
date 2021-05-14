67e29f537518e43083bf969ed60185c0d7eb1a7d
Ricardo Deniso Garcia <74632026+ricardodenisogarcia@users.noreply.github.com>
2021-05-14 22:07:45 +0100

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 class Departamento {
   nombre: string;
   dueño: string;
-  constructor(nombre: string, dueño: string) {
+  constructor(nombre: string) {
     this.nombre = nombre;
-    this.dueño = dueño;
   }
   getName() {
     return this.nombre;
@@ -11,13 +10,12 @@ class Departamento {
 }
 class Piso {
   nombre: string;
-  departamentos: Departamento[];
+  departamentos: Departamento[] = []; //
   constructor(nombre: string) {
     this.nombre = nombre;
   }
-  pushDepartamento(nombre: string, dueño: string) {
-    var nuevoDepto = new Departamento(nombre, dueño);
-    // console.log(nuevoDepto); //
+  pushDepartamento(nombre: string) {
+    var nuevoDepto = new Departamento(nombre);
     return this.departamentos.push(nuevoDepto);
   }
   getDepartamentos() {
@@ -28,34 +26,59 @@ class Piso {
 //un método addDepartamentoToPiso(nombreDePiso:string, departamento:Departamento)
 //que reciba un Departamento y lo ubique en el piso con el nombre indicado,
 //si no existe un piso con ese nombre debe mostrar un error.
-/*class Edificio {
-  pisos:string[];
-  constructor(Piso:string[]){}
-  function addDepartamentoToPiso(nombreDePiso:string, departamento:Departamento){
-  var pisoIndicado = Piso.find(function(item:any){
-    if(item.nombre == nombreDePiso){
-      return item
-        }else{return console.log("El piso no existe")}
-  });
-  return Piso[pisoIndicado].departamentos.push(departamento)
-  };
-//un método getDepartamentosByPiso(nombreDePiso:string) que devuelva todos los Departamento(s) de un piso.
-function getDepartamentosByPiso(nombreDePiso:string){
-  var pisoCorrecto = Piso.find(function(item:any){
-if(item.nombre == nombreDePiso){
-  return item
+class Edificio {
+  pisos: string[] = [];
+  constructor(nombreDelPiso: any) {
+    this.pisos = nombreDelPiso;
+  }
+  addDepartamentoToPiso(nombreDePiso: string, departamento: Departamento) {
+    var pisoIndicado = this.pisos.find(function (item: any) {
+      if (item.nombre == nombreDePiso) {
+        return item;
+      } else {
+        return console.log("El piso no existe");
+      }
+    });
+    return Piso[pisoIndicado].departamentos.push(departamento);
+  }
+  //un método getDepartamentosByPiso(nombreDePiso:string) que devuelva todos los Departamento(s) de un piso.
+  getDepartamentosByPiso(nombreDePiso: string) {
+    var pisoCorrecto = this.pisos.find(function (item: any) {
+      if (item.nombre == nombreDePiso) {
+        return item;
+      }
+    });
+    return Piso[pisoCorrecto].departamentos;
+  }
 }
-  });
-  return Piso[pisoCorrecto].departamentos
-}
-  
 
+function testClaseEdificio() {
+  const unPiso = new Piso("planta baja");
+  const otroPiso = new Piso("primer piso");
+  const unEdificio = new Edificio([unPiso, otroPiso]);
+  const deptoUno = new Departamento("depto uno");
+  const deptoDos = new Departamento("depto dos");
+  const deptoTres = new Departamento("depto tres");
+  unEdificio.addDepartamentoToPiso("planta baja", deptoUno);
+  unEdificio.addDepartamentoToPiso("planta baja", deptoDos);
+  unEdificio.addDepartamentoToPiso("planta baja", deptoTres);
+
+  const deptos = unEdificio.getDepartamentosByPiso("planta baja");
+  const deptosEmpty = unEdificio.getDepartamentosByPiso("primer piso");
+
+  if (
+    Array.isArray(deptosEmpty) &&
+    deptosEmpty.length == 0 &&
+    deptos.length == 3 &&
+    deptos[2].getName() == "depto tres"
+  ) {
+    console.log("testClaseBandaApartment passed");
+  } else {
+    throw "testClaseBandaApartment not passed";
+  }
 }
-*/
-var depto1 = new Departamento("Mi piso", "Ricardito");
-console.log(depto1);
-console.log(depto1.getName());
-var piso7 = new Piso("Septimo piso");
-console.log("soy el piso7", piso7);
-piso7.pushDepartamento("Septimo D", "Susana");
-console.log("soy el piso 7 actualizado", piso7.getDepartamentos);
+
+function main() {
+  testClaseEdificio();
+}
+main();
